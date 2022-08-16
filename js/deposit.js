dc4e2af14fd654dbe7975fbbccfff61cb02eb58c
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-08-16 17:29:53 +0600

    Login or Deposit Banking Done

---
+++ js/deposit.js
@@ -0,0 +1,29 @@
+// Step-1: add Event listener to the deposit button
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    // Step-2: get the deposit amount from the deposit input field
+    // For input field use. value to the value inside the input filed
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    // Step-3: get the current deposit total
+    // for non-input(element other than input, textarea) use innertext to get the text
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    // Step-4: add number to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // Step-5: get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // Step-6: calclute current total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // Clear the deposit field
+    depositField.value = '';
+});
---
+++ js/login.js
@@ -0,0 +1,25 @@
+// Step-1: add click event handler with the submit button
+document.getElementById('submit-btn').addEventListener('click', function () {
+    // Step-2: get the email address inside the email input field
+    // alwys remember to use .value to get text from an input field
+    const userEmail = document.getElementById('user-email');
+    const email = userEmail.value;
+
+    /*
+        Step-3: get password
+        3.a: set id on the html element
+        3.b: get the element
+        3.c: get the value from the element
+    */
+    const userPassword = document.getElementById('user-password');
+    const password = userPassword.value;
+
+    // DANGER: DO NOT VERFY email password to the client side
+    // Step-4: verfy email and password
+    if (email === "noyonhossain560@gmail.com" && password === "secrete") {
+        window.location.href = "bank.html";
+    }
+    else {
+        alert("Invalid Your Information");
+    }
+});
